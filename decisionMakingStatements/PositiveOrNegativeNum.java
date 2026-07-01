d7768f2b7e16ad842528c83c0015e85223a01314
natasha-patil <natashapatil7211@gmail.com>
2026-07-01 20:21:00 +0530

    program for checking number is positive or negative using if else statement

---
+++ decisionMakingStatements/PositiveOrNegativeNum.java
@@ -0,0 +1,20 @@
+package decisionMakingStatements;
+
+public class PositiveOrNegativeNum {
+ public static void main(String[] args) {
+	int num=88;
+	/*
+	 int num=0;
+	 int num=-88;
+	 */
+	
+	if(num>=0)
+	{
+		System.out.println(num+" Number is Positive");
+	}
+	else
+	{
+		System.out.println(num+" Number is Negative");
+	}
+}
+}
