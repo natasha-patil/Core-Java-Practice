3d9372e121ebc52bfeaa375030fb8804e6eaee55
natasha-patil <natashapatil7211@gmail.com>
2026-07-01 20:26:35 +0530

    program to check number is even or odd using if else statement

---
+++ decisionMakingStatements/EvenOrOddNum.java
@@ -0,0 +1,16 @@
+package decisionMakingStatements;
+
+public class EvenOrOddNum {
+	public static void main(String[] args) {
+		int num=20;
+		if(num%2==0)
+		{
+			System.out.println(num+" is Even Number");
+		}
+		else
+		{
+			System.out.println(num+" is Odd Number");
+		}
+	}
+
+}
