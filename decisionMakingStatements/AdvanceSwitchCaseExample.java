4066f811691c7dffa0bcf9b4b7f791f73a2b11a9
natasha-patil <natashapatil7211@gmail.com>
2026-07-02 22:34:51 +0530

    Advance switch case example

---
+++ decisionMakingStatements/AdvanceSwitchCaseExample.java
@@ -0,0 +1,28 @@
+package decisionMakingStatements;
+
+public class AdvanceSwitchCaseExample {
+  public static void main(String[] args) {
+	char gen='f';
+	switch(gen) {
+	case 'm'->
+	 System.out.println("male");
+	case 'f'->
+	 System.out.println("Female");
+	case 'o'->
+	 System.out.println("Other");
+	default ->
+	 System.out.println("Invalid gender");
+	}
+	 /* switch(gen) {
+	case 'm','M' ->
+	 System.out.println("male");
+	case 'f','F' ->
+	 System.out.println("Female");
+	case 'o','O' ->
+	 System.out.println("Other");
+	default ->
+	 System.out.println("Invalid gender");
+	}*/
+	 
+} 
+}
